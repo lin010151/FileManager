de238dd48da30f4a0108c9df00559a7560e6b2c4
lin010151 <lin010151@hotmail.com>
2016-06-10 22:08:37 +0800

    Add hint to searchable.

--- app/src/main/java/cn/edu/gdei/filemanager/main/SearchableActivity.java
+++ app/src/main/java/cn/edu/gdei/filemanager/main/SearchableActivity.java
@@ -2,9 +2,12 @@ package cn.edu.gdei.filemanager.main;
 
 import android.app.SearchManager;
 import android.content.Intent;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import cn.edu.gdei.filemanager.R;
@@ -21,7 +24,9 @@ public class SearchableActivity extends AppCompatActivity {
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
             doSearch(query);
+
         }
+
     }
 
     @Override
@@ -37,7 +42,24 @@ public class SearchableActivity extends AppCompatActivity {
 
     private void doSearch(String query) {
         // TODO: 2016/6/10 根据query搜索
-        TextView textView = (TextView) findViewById(R.id.searchable_text_view);
-        textView.setText(query);
+        int quantity = 0;
+        showHint(query, quantity);
+    }
+
+    private void showHint(String query, int quantity) {
+        String message;
+        if (quantity == 0) {
+            message = getString(R.string.no_results, new Object[] {query});
+        } else {
+            message = getResources().getQuantityString(R.plurals.search_results, quantity, new Object[] {quantity, query});
+        }
+        final Snackbar snackbar = Snackbar.make(findViewById(R.id.coordinatorLayout), message, Snackbar.LENGTH_INDEFINITE);
+        snackbar.setAction(getString(R.string.search_hint_dismiss), new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                snackbar.dismiss();
+            }
+        });
+        snackbar.show();
     }
 }
