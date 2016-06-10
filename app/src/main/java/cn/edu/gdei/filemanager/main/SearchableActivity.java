c9ffa77850e1c3176ceae12ef9a95a89cd25f096
lin010151 <lin010151@hotmail.com>
2016-06-10 15:11:32 +0800

    Preparations for searchable.

---
+++ app/src/main/java/cn/edu/gdei/filemanager/main/SearchableActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.gdei.filemanager.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.edu.gdei.filemanager.R;
+
+public class SearchableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchable);
+    }
+}
